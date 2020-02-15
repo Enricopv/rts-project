0e11e2440e41c501b29890d3c5f4ace1b1d2d144
Enrico <enrico@valpunk.com>
2020-02-15 15:29:56 -0800

    add UI Health bars

---
+++ Assets/Scripts/Health_UI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health_UI : MonoBehaviour
+{
+  public GameObject uiPrefab;
+  public Transform target;
+  // Start is called before the first frame update
+  Transform cam;
+  Transform ui;
+  Image healthSlider;
+  void Start()
+  {
+    cam = Camera.main.transform;
+    foreach (Canvas c in FindObjectsOfType<Canvas>())
+    {
+      if (c.name == "UnitCanvas")
+      {
+        ui = Instantiate(uiPrefab, c.transform).transform;
+        healthSlider = ui.GetChild(0).GetComponent<Image>();
+        break;
+      }
+    }
+
+  }
+
+  // Update is called once per frame
+  void LateUpdate()
+  {
+    ui.position = target.position;
+
+
+    ui.forward = -cam.forward;
+
+  }
+}
